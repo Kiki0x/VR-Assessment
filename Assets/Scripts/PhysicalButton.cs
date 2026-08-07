e8d90e63ce8457c6f6dcd452ffd810839cafbaea
Kiki <152764246+Kiki0x@users.noreply.github.com>
2026-08-08 03:09:38 +0530

    PhysicalButton.cs

--- Assets/Scripts/PhysicalButton.cs
+++ Assets/Scripts/PhysicalButton.cs
@@ -5,21 +5,16 @@ using UnityEngine.Events;
 
 public class PhysicalButton : MonoBehaviour
 {
-    public UnityEvent onPoke; // This lets us wire it in the Inspector just like a UI button!
+    public UnityEvent onPoke;
 
     private bool isCooldown = false;
 
     void OnTriggerEnter(Collider other)
     {
-        // Only trigger if we aren't in cooldown
         if (!isCooldown)
         {
-            // Optional: You could check if(other.CompareTag("PlayerHand")) here, 
-            // but for a quick assessment, any collision is fine!
+            onPoke.Invoke(); 
 
-            onPoke.Invoke(); // Trigger the GameManager function!
-
-            // Add a tiny cooldown so we don't accidentally double-click
             isCooldown = true;
             Invoke("ResetCooldown", 1.0f);
         }
